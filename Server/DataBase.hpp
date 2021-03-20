63e2fb47d5c82bf74b5d2203ecfcd968e4e6820b
Sajjad <sssanikhani@gmail.com>
2021-03-21 01:29:14 +0330

    feat: add ClientManager class to DataBase for managing clients

--- Server/DataBase.hpp
+++ Server/DataBase.hpp
@@ -13,15 +13,31 @@ public:
     {
     public:
         static User get(std::string username);
-        static User put(User user);
+        static User add(User user);
         static User remove(std::string username);
         static std::vector<User> get_all();
 
     private:
         static std::unordered_map<std::string, User> users;
     };
+
+    class ClientManager
+    {
+    public:
+        static int bind_socket_to_user(string username, int sd);
+        static int authenticate(string username, int sd);
+        static bool is_authenticated(int sd);
+        static User get_user(int sd);
+        static int logout();
+
+    private:
+        static std::unordered_map<int, User> socket_user_map;
+        static std::unordered_map<int, bool> socket_authentication;
+    };
 };
 
 std::unordered_map<std::string, User> users;
+std::unordered_map<int, User> socket_user_map;
+std::unordered_map<int, bool> socket_authentication;
 
 #endif
