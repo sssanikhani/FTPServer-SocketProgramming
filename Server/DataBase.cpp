ce7b7accefdf835e648bef50dafbe107d6f9a01b
Sajjad <sssanikhani@gmail.com>
2021-03-20 19:42:15 +0330

    fix: format DataBase.cpp

--- Server/DataBase.cpp
+++ Server/DataBase.cpp
@@ -4,17 +4,18 @@
 #include "DataBase.hpp"
 using namespace std;
 
-User DataBase::UserManager::get(string username) {
+User DataBase::UserManager::get(string username)
+{
 }
 
-User DataBase::UserManager::put(User user) {
-
+User DataBase::UserManager::put(User user)
+{
 }
 
-User DataBase::UserManager::remove(string username) {
-
+User DataBase::UserManager::remove(string username)
+{
 }
 
-vector<User> DataBase::UserManager::get_all() {
-
+vector<User> DataBase::UserManager::get_all()
+{
 }
