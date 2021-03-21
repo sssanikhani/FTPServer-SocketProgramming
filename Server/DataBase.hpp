30e256292e7634ec5877dc7dbb032ce6ffb598ae
Sajjad <sssanikhani@gmail.com>
2021-03-22 01:33:11 +0430

    fix: change definition of static private members for DataBase class managers

--- Server/DataBase.hpp
+++ Server/DataBase.hpp
@@ -37,8 +37,8 @@ public:
     };
 };
 
-std::unordered_map<std::string, User> users;
-std::unordered_map<int, std::string> socket_username_map;
-std::unordered_map<int, bool> socket_authentication;
+std::unordered_map<std::string, User> DataBase::UserManager::users;
+std::unordered_map<int, std::string> DataBase::ClientManager::socket_username_map;
+std::unordered_map<int, bool> DataBase::ClientManager::socket_authentication;
 
 #endif
