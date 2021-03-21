ae37088e8600ecf29ca7ffbcaf5d7d0d16b738f0
Sajjad <sssanikhani@gmail.com>
2021-03-21 11:08:58 +0330

    feat: add implementations of DataBase methods + some changing in structure

--- Server/DataBase.cpp
+++ Server/DataBase.cpp
@@ -1,21 +1,80 @@
+#include "DataBase.hpp"
+#include "User.hpp"
 #include <string>
 #include <unordered_map>
 #include <vector>
-#include "DataBase.hpp"
+#include <unistd.h>
 using namespace std;
 
-User DataBase::UserManager::get(string username)
+User &DataBase::UserManager::get(string username)
 {
+    if (exists(username))
+        return users[username];
+    return;
 }
 
-User DataBase::UserManager::put(User user)
+User &DataBase::UserManager::add(User user)
 {
+    users[user.get_username()] = user;
+    return users[user.get_username()];
 }
 
-User DataBase::UserManager::remove(string username)
+bool DataBase::UserManager::exists(string username)
 {
+    return users.find(username) != users.end();
 }
 
-vector<User> DataBase::UserManager::get_all()
+vector<User *> DataBase::UserManager::get_all()
 {
+    vector<User *> res;
+    for (auto it = users.begin(); it != users.end(); it++)
+    {
+        res.push_back(&(it->second));
+    }
+    return res;
+}
+
+int DataBase::ClientManager::bind_socket_to_user(string username, int sd)
+{
+    socket_username_map[sd] = username;
+    socket_authentication[sd] = false;
+    return sd;
+}
+
+int DataBase::ClientManager::authenticate(string username, int sd)
+{
+    bind_socket_to_user(username, sd);
+    socket_authentication[sd] = true;
+    return sd;
+}
+
+bool DataBase::ClientManager::is_authenticated(int sd)
+{
+    if (socket_authentication.find(sd) == socket_authentication.end())
+        return false;
+
+    return socket_authentication[sd];
+}
+
+bool DataBase::ClientManager::is_bound(int sd)
+{
+    if (socket_username_map.find(sd) == socket_username_map.end())
+        return false;
+    return true;
+}
+
+string DataBase::ClientManager::get_username(int sd)
+{
+    if (!is_bound(sd))
+        return NULL;
+    return socket_username_map[sd];
+}
+
+void DataBase::ClientManager::logout(int sd)
+{
+    if (socket_authentication.find(sd) != socket_authentication.end())
+        socket_authentication.erase(sd);
+
+    if (socket_username_map.find(sd) != socket_username_map.end())
+        socket_username_map.erase(sd);
 }
--- Server/DataBase.hpp
+++ Server/DataBase.hpp
@@ -12,10 +12,10 @@ public:
     class UserManager
     {
     public:
-        static User get(std::string username);
-        static User add(User user);
-        static User remove(std::string username);
-        static std::vector<User> get_all();
+        static User& get(std::string username);
+        static User& add(User user);
+        static std::vector<User*> get_all();
+        static bool exists(std::string username);
 
     private:
         static std::unordered_map<std::string, User> users;
@@ -24,20 +24,21 @@ public:
     class ClientManager
     {
     public:
-        static int bind_socket_to_user(string username, int sd);
+        static int bind_socket_to_user(std::string username, int sd);
         static int authenticate(string username, int sd);
         static bool is_authenticated(int sd);
-        static User get_user(int sd);
-        static int logout();
+        static bool is_bound(int sd);
+        static std::string get_username(int sd);
+        static void logout(int sd);
 
     private:
-        static std::unordered_map<int, User> socket_user_map;
+        static std::unordered_map<int, std::string> socket_username_map;
         static std::unordered_map<int, bool> socket_authentication;
     };
 };
 
 std::unordered_map<std::string, User> users;
-std::unordered_map<int, User> socket_user_map;
+std::unordered_map<int, std::string> socket_username_map;
 std::unordered_map<int, bool> socket_authentication;
 
 #endif
