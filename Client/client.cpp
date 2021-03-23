a92dae4448fcf335cf3a2b5aeedbadd24fdc01ae
Sajjad <sssanikhani@gmail.com>
2021-03-24 03:22:12 +0430

    feat: add data socket to client to get data from server

--- Client/client.cpp
+++ Client/client.cpp
@@ -8,61 +8,95 @@
 #define PORT 3000
 #define TRUE 1
 
+#define TEST_RES1 "226: Successful Download."
+#define TEST_RES2 "226: List transfer done."
+
 using namespace std;
 
 int main()
 {
-    struct sockaddr_in serv_addr;
+    struct sockaddr_in command_serv_addr;
+    struct sockaddr_in data_serv_addr;
 
     char buffer[1024] = {0};
     int opt = 1;
 
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
+    int command_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    int data_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (command_sockfd < 0 || data_sockfd < 0)
     {
-        cout << "Unable to create Socket!\n";
-        return 1;
+        perror("socket");
+        exit(EXIT_FAILURE);
     }
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
+    command_serv_addr.sin_family = AF_INET;
+    command_serv_addr.sin_port = htons(PORT); // TODO: get from config.json
 
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+    data_serv_addr.sin_family = AF_INET;
+    data_serv_addr.sin_port = htons(8001); // TODO: get from config.json
+
+    if (inet_pton(AF_INET, "127.0.0.1", &command_serv_addr.sin_addr) <= 0)
     {
-        printf("\nInvalid address/ Address not supported \n");
-        return -1;
+        perror("invalid address");
+        exit(EXIT_FAILURE);
     }
 
-    int connectfd = connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-    if (connectfd < 0)
+    if (inet_pton(AF_INET, "127.0.0.1", &data_serv_addr.sin_addr) <= 0)
     {
-        cout << "Unable to Connect!\n";
-        return 1;
+        perror("invalid address");
+        exit(EXIT_FAILURE);
     }
 
-    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+    int command_connectfd = connect(command_sockfd, (struct sockaddr *)&command_serv_addr, sizeof(command_serv_addr));
+    int data_connectfd = connect(data_sockfd, (struct sockaddr *)&data_serv_addr, sizeof(data_serv_addr));
+    if (command_connectfd < 0 || data_connectfd < 0)
+    {
+        perror("connect");
+        exit(EXIT_FAILURE);
+    }
+    cout << "Connected Successfully!" << endl;
+
+    if (setsockopt(command_sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    opt = 1;
+    if (setsockopt(data_sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
     {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
 
     int valread;
-    string input;
+    string server_message, server_data;
     char delim = '\n';
     while (TRUE)
     {
-        valread = recv(sockfd, buffer, 1024, 0);
+        valread = recv(command_sockfd, buffer, sizeof(buffer), 0);
         if (valread <= 0)
         {
             perror("recv");
+            continue;
         }
         buffer[valread] = '\0';
-        cout << buffer;
-        buffer[0] = '\0';
+        server_message = buffer;
+        cout << server_message << endl;
+        if (server_message == TEST_RES1 || server_message == TEST_RES2){
+            valread = recv(data_sockfd, buffer, sizeof(buffer), 0);
+            if (valread <= 0)
+            {
+                perror("recv");
+                continue;
+            }
+            server_data = buffer;
+            cout << server_data << endl;
+        }
         char char_input[100] = "";
         cin.getline(char_input, sizeof(char_input), '\n');
         cout << "char_input:" << char_input << endl;
-        if (send(sockfd, char_input, strlen(char_input), 0) <= 0)
+        if (send(command_sockfd, char_input, strlen(char_input), 0) <= 0)
         {
             perror("send");
         }
