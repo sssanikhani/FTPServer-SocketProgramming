7a82777603f511850cefdf6e7d1fb79623fc548c
Sajjad <sssanikhani@gmail.com>
2021-03-24 02:06:30 +0430

    feat: add file_to_string util + feat: add join() util to join strings

--- Utils/utils.cpp
+++ Utils/utils.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <fstream>
 #include "utils.hpp"
 using namespace std;
 
@@ -14,4 +15,26 @@ vector<string> split(string s)
         res.push_back(word);
 
     return res;
+}
+
+string join(const vector<string> &v, char c)
+{
+    string s = "";
+    for (vector<string>::const_iterator p = v.begin(); p != v.end(); ++p)
+    {
+        s += *p;
+        if (p != v.end() - 1)
+            s += c;
+    }
+    return s;
+}
+
+string file_to_string(const string &path)
+{
+    auto ss = ostringstream{};
+    ifstream input_file(path);
+    if (!input_file.is_open())
+        return "ERROR: can not read file";
+    ss << input_file.rdbuf();
+    return ss.str();
 }
--- Utils/utils.hpp
+++ Utils/utils.hpp
@@ -5,5 +5,7 @@
 #include <string>
 
 std::vector<std::string> split(std::string);
+std::string join(const std::vector<std::string> &v, char c);
+std::string file_to_string(const std::string &path);
 
 #endif
